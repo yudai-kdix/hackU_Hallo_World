fb550a1fd58936156d2d94eef5f0632a51541b64
Kento1225 <yassanpe@softbank.ne.jp>
2023-09-11 22:51:42 +0900

    add
    APIキーを新しくしました

--- app/controllers/api/gpt_controller.rb
+++ app/controllers/api/gpt_controller.rb
@@ -6,10 +6,11 @@ Dotenv.load
   before_action :set_common_variable
 
   private
+  HACKU_API_KEY = ENV["API_KEY"]
 
   # APIキーの設定
   def set_common_variable
-    @client = OpenAI::Client.new(access_token: "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")
+    @client = OpenAI::Client.new(access_token: HACKU_API_KEY)
   end
 end
 
