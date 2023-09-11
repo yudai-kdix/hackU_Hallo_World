dc4f4cb9b80c90e6e4d885e59bd8d24d56bc43d9
SS0117 <senju.0117@icloud.com>
2023-09-12 01:19:55 +0900

    [Add]printsコントローラーの編集とUser/showの追加

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -2,4 +2,20 @@ class PrintsController < ApplicationController
     def index
         @prints=Print.all
     end
+
+    def new
+    end
+
+    def create
+        @print = Print.new(print_params)
+        @print.user_id = current_user.id
+        @print.save
+        redirect_to ""
+    end
+
+    private
+
+    def print_params
+        params.require(:print).permit(:)
+    end
 end
