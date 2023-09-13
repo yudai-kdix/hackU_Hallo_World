1845b53489874a80006c170f76ab10fee2bf732e
Kento1225 <yassanpe@softbank.ne.jp>
2023-09-13 22:29:12 +0900

    add
    とりあえずAPIを通してGPTに質問できるようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'prints/index' => 'prints#index'
   get 'chats/Chatgptedit' => 'chats#Chatgptedit'
   get 'chats/search' => 'chats#search'
+   
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
