93efd4e3ca4757e434d2809f68c811aacd20adad
yudai-kdix <yudai.homma222@gmail.com>
2023-09-15 21:50:06 +0900

    add pdf

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,15 +6,16 @@ class ChatsController < GptController
     def Chatgptedit
     end
   
- 
+
     def search
       @query = params[:query]
   
       response = @client.chat(
         parameters: {
             model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: @query }],
-           
+            messages: [
+                        { role: "system", content: "今から与える文章について4択の問題を作り答えてください" },
+                        { role: "user", content: @query }],
         })
   
       @chats = response.dig("choices", 0, "message", "content")
